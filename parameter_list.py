3f7f40aae250a3f7048e39bd10306241b91a9913
frbosse <30830914+frbosse@users.noreply.github.com>
2019-04-09 10:18:06 +0200

    add small changes for assignment

--- parameter_list.py
+++ parameter_list.py
@@ -4,13 +4,13 @@ import pygame
 from math import atan2,degrees
 
 
-#Initialise pygame
+# Initialise pygame
 pygame.init()
-#small screen size
+# small screen size
 SCREENSIZE = (800,600)
-#Get current resolution of screen
+# Get current resolution of screen
 infoObject = pygame.display.Info()
-#set screen size based on size of monitor
+# set screen size based on size of monitor
 SCREEN = pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
 # SCREEN = pygame.display.set_mode(SCREENSIZE)
 # SCREEN = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
@@ -26,43 +26,45 @@ distance = 30
 ydegree_per_px = degrees(atan2(.5*HEIGHT, distance)) / (.5*infoObject.current_h)
 xdegree_per_px = degrees(atan2(.5*WIDTH, distance)) / (.5*infoObject.current_w)
 
-#set radius of the stimulus circle
+# set radius of the stimulus circle
 RADIUS = round(5/ydegree_per_px)
 
-#set center point of the screen
+# set center point of the screen
 (Cx,Cy) = (int(round(WIDTH/2)), int(round(HEIGHT/2)))
 
-#set length of the fixation cross
+# set length of the fixation cross
 crosslen = 1.6/ydegree_per_px
 VLINE = [(Cx,Cy - crosslen), (Cx, Cy + crosslen), 4]
 HLINE = [(Cx - crosslen,Cy), (Cx + crosslen, Cy), 4]
 
-#Default colors for the stimulus and text
+# Default colors for the stimulus and text
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 NOGO_COLOR = (255, 0 , 0)
 GO_COLOR = (0, 255, 0)
 
-#Set color of background
+# Set color of background
 BG_COLOR = (123,123,123)
 
-#Set fontsize of the text
+# Set fontsize of the text
 FONTSIZE = 60
 
-#Set number of trials in the experiment
-NUMTRIAL = 10
+# Set number of trials in the experiment
+# How should you name the variable?
+# TODO
 
-#set number of nogo trials in the experiment
-PCT_NOGO = 0.5
+# Set number of nogo trials in the experiment
+# How should you name the variable?
+# TODO
 
-#Set the time interval in seconds of the delay
-#from end of trial n and beginning of trial n+1
+# Set the time interval in seconds of the delay
+# from end of trial n and beginning of trial n+1
 TRIALINTERVAL = 1
 
-#Frames per second of the experiment
+# Frames per second of the experiment
 FPS = 60
 
-# Make the DATA folder where the experiment data for each subject is saved
-DATAPATH = os.getcwd() + '/Data'
-if not os.path.exists(DATAPATH):
-    os.makedirs(DATAPATH)
+# Create a directory called 'Data' in your working-directory
+# where the experiment data for each subject is saved
+# before creating it check if the directory 'Data' already exists
+# TODO
