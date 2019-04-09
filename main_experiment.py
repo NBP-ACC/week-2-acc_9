68f5c15c54037de3ded0332da0e26c64324e59ca
Ashima Keshava <ashimakeshava@gmail.com>
2019-04-09 15:42:57 +0200

    Updated to new version

--- main_experiment.py
+++ main_experiment.py
@@ -1,67 +1,23 @@
 import pygame
-import time
 import sys
 import csv
 import random
-from  parameter_list import *
+from parameter_list import *
+clock = pygame.time.Clock()
 
-
-def draw_stim(trialType):
+def draw_stimulus(trialType):
     """
     Function to draw the stimulus
     green circle for trialType == 1
     red circle for trialType == 0
-    The subject response is recorded by getting pygame.event which keeps track of button presses
-    The function counts down 2 seconds for the subject to respond from the
-    beginning of the stimulus presentation. If there is no response,
-    the function returns to the main loop. If the subject responds, the Function
-    returns the response and the time from stimulus presentation to time at
-    button press
-
     parameters: trialType
-    returns: response and response_time
     """
-    response = 0 # should be assigned 1 if K_SPACE is pressed
-    RT = 0 # should be assigned value based on elapsed time from when stimulus is shown
-    countdown = 2
-    if trialType == 0:
+    if trialType:
         SCREEN.fill(BG_COLOR)
-        pygame.draw.circle(SCREEN,NOGO_COLOR, [Cx, Cy], int(round(RADIUS)), 0)
-        # flip the screen to display the drawn stimulus
-        pygame.display.flip()
-        start = time.time() #start timer when the stimulus is shown
-
-
+        pygame.draw.circle(SCREEN,GO_COLOR, [Cx, Cy], RADIUS, 0)
     else:
         SCREEN.fill(BG_COLOR)
-        pygame.draw.circle(SCREEN,GO_COLOR, [Cx, Cy], int(round(RADIUS)), 0)
-        # flip the screen to display the drawn stimulus
-        pygame.display.flip()
-        start = time.time() #start timer when the stimulus is shown
-
-    #start countdown to end of 2s or until a button is pressed
-    # pygame.USEREVENT is a custom event handler.
-    #In the code below it updates itself every 1000ms
-    pygame.time.set_timer(pygame.USEREVENT,1000)
-    while not countdown <= 0 or res != 1:
-        for event in pygame.event.get():
-            # if the pygame exit button is pressed
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            # 1000ms have passed and USEREVENT has occurred
-            if event.type == pygame.USEREVENT:
-                countdown -= 1 # reduce the countdown
-            # Subject has pressed a button
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    # TODO
-                    # What should happen when the space key is pressed?
-                    
-        #clear the event buffer so multiple button presses are ignored
-        pygame.event.clear()
-        return (RT, response)
-
+        pygame.draw.circle(SCREEN,NOGO_COLOR, [Cx, Cy], RADIUS, 0)
 
 def message_display(text):
     """
@@ -95,6 +51,8 @@ def message_display(text):
         return 1
 
 
+
+#draw fixation cross
 def draw_fixation():
     """
     Function to draw fixation cross based on the parameters listed in
@@ -103,15 +61,15 @@ def draw_fixation():
     SCREEN.fill(BG_COLOR)
     pygame.draw.line(SCREEN,WHITE, VLINE[0], VLINE[1],VLINE[2])
     pygame.draw.line(SCREEN,WHITE, HLINE[0], HLINE[1],HLINE[2])
-    pygame.display.flip()
-    return 1
 
+def fill_background():
+    SCREEN.fill(BG_COLOR)
 
+#write the data into csv file
 def writeData(datalist, subID):
     """
     Function to write the list of responses to a csv dataFile
     """
-
     # create a csvfile for each subject and name it: Sub[subID].csv
     # add a header ('SubjectID','StimulusType','response','RT') to the csvfile
     # and write each entry of datalist to a single row
@@ -120,9 +78,8 @@ def writeData(datalist, subID):
 
 ######                 main experiment loop            ##########
 def experiment(subID):
-    #List where all the repsonses are stores
+    #List where all the repsonses are stored
     dataFile = []
-    #round(random.uniform(1.75,2.75), 2)
     pygame.mouse.set_visible(False)
     stimuli_list = [1]*int(NUMTRIAL- NUMTRIAL*PCT_NOGO)
     nogo_trials = [0]*int(NUMTRIAL*PCT_NOGO)
@@ -135,31 +92,55 @@ def experiment(subID):
         code = message_display(text)
         if code == 1:
             for stim in stimuli_list:
+                response = 0 # should be assigned 1 if K_SPACE is pressed
+                RT = 0 # should be assigned value based on elapsed time from when stimulus is shown
+                countdown = 2
                 draw_fixation()
-                time.sleep(0.5)
+                pygame.display.flip()
+                pygame.time.wait(500) # Display fixation cross for 500 milliseconds
                 #clear event buffer so they are not misunderstood as responses
                 pygame.event.clear(pygame.KEYDOWN)
                 #show stimulus and get RT and response
-                RT, res = draw_stim(stim)
-                #append the data to the datafile
-                dataFile.append([subID, stim, res, RT])
-                time.sleep(TRIALINTERVAL)
+                draw_stimulus(stim)
+                pygame.display.flip()
+                # get time at which stimulus is shown
+                start = pygame.time.get_ticks()
+                # check for events
+                countdown_check = pygame.USEREVENT+1 #custom event to track counter
+                pygame.time.set_timer(countdown_check, 1000) # timer that tracks counter every 1000ms
+                while countdown > 0 and response == 0:
+                    clock.tick(FPS)
+                    for event in pygame.event.get():
+                        # if the pygame exit button is pressed
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        # if 1000ms have passed do a countdown check
+                        if event.type == countdown_check:
+                            countdown -= 1
+                        # if subject has pressed a button
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_SPACE:
+                                # Time elapsed from stimulus to button press
+                                RT = # TODO
+                                response = # TODO
 
-            # when all stimulus is shown, experiment is done
-            done = True
-            # check for events
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    pygame.display.set_mode(SCREENSIZE)
+                fill_background()# clear the screen
+                pygame.display.flip()
+                pygame.time.wait(TRIALINTERVAL)
+                dataFile.append([subID, stim, response, RT]) #append the data to the datafile
+
+        done = True
 
     return dataFile
 
-# Give information about the subject and start experiment
 if __name__ == "__main__":
     #Fill this before start of the experiment
-    subID = # ID of the subject
+    subID = # TODO ID of the subject
     dataFile = experiment(subID)
-    print('writing in data file')
+    print('*'*30)
+    print('Writing in data file: Sub{}.csv'.format(subID))
+    print('*'*30)
     writeData(dataFile, subID)
     pygame.quit()
     quit()
